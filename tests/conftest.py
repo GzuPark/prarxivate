133092f1fd07d4c02da4df3e4bd1ada91a6bfb8a
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2019-11-27 18:24:03 +0900

    Remove github actions (#26)
    * Add a file for testing github actions
    
    * Delete a file for testing github actions
    
    * Remove github actions

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from fetch_papers import get_args as fetch_args
-
-@pytest.fixture()
-def get_fetch_args():
-    args = fetch_args()
-    args.max_index = 100
-    return args
--- tests/unit/test_fetch.py
+++
@@ -1,5 +0,0 @@
-from fetch_papers import fetch
-
-
-def test_fetch_default(get_fetch_args):
-    fetch(get_fetch_args)
